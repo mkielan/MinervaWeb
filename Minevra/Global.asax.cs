3ce9304b065c4e63844f1c8264c2e2438ddd45f4
Mariusz Kielan <mariusz@kielan.org>
2014-04-01 00:57:58 +0200

    Add windsor IoC container, but it is off.

--- Minevra/Global.asax.cs
+++ Minevra/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,8 @@ namespace Minevra
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer _container;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +22,26 @@ namespace Minevra
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // todo close install windsor container
+        }
+
+        /// <summary>
+        /// Installl Windsor Castole IoC container
+        /// </summary>
+        private static void InjectContainer()
+        {
+            _container = new WindsorContainer().Install(FromAssembly.This());
+
+            // todo register classes
+        }
+
+        /// <summary>
+        /// Destroy Windsor Castle IoC container
+        /// </summary>
+        protected void Application_End()
+        {
+            _container.Dispose();
         }
     }
 }
