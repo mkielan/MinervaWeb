edac120aa12b8f6c280296bb87385fa2c3de9e1b
Mariusz Kielan <mariusz@kielan.org>
2014-04-08 01:45:21 +0200

    First part of change project name.

---
+++ Minerva.Services/INodeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Minevra.Services.Contracts;
+
+namespace Minevra.Services
+{
+    [ServiceContract]
+    public interface INodeService
+    {
+        [OperationContract]
+        IList<ItemInfo> GetChildrenInfo(int? parentId);
+
+        [OperationContract]
+        IList<Item> GetChildren(int? parentId);
+
+        [OperationContract]
+        void RemoveItem(int itemId);
+
+        [OperationContract]
+        void AddItem(int parentId, Item item);
+
+        [OperationContract]
+        void ChangeItem(Item item);
+
+        [OperationContract]
+        IList<ItemProperties> GetProperties(int itemId);
+    }
+}
---
+++ Minerva.Services/NodeService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Minevra.Services.Contracts;
+
+namespace Minevra.Services
+{
+    public class NodeService : INodeService
+    {
+        public IList<ItemInfo> GetChildrenInfo(int? parentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Item> GetChildren(int? parentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItem(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddItem(int parentId, Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ItemProperties> GetProperties(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
