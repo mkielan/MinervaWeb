78ea475075f0464614b6329f891834cfaac64c09
Mariusz Kielan <mariusz@kielan.org>
2014-05-04 01:44:21 +0200

    Change in the models

--- Minerva.Entities/Sources/Internal/DiskStructure.cs
+++ Minerva.Entities/Sources/Internal/DiskStructure.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace Minerva.Entities.Sources.Internal
 {
     public class DiskStructure : AbstractEntity 
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Path { get; set; }
+
+        [Column("Parent")]
+        public DiskStructure Parent { get; set; }
+
+        public virtual ICollection<DiskStructure> Children { get; set; }
+
+        public Directory Directory { get; set; }
+
+        public File File { get; set; }
     }
 }
