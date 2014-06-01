90d1b6a995451f60946268627e3edfb90ab57238
Mariusz Kielan <mariusz@kielan.org>
2014-06-01 19:28:25 +0200

    Added missing files

---
+++ Minerva.Common/FileStorage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOFile = System.IO.File;
+
+namespace Minerva.Common
+{
+    /// <summary>
+    /// Helper do zarządzanie przestrzenią dyskową.
+    /// </summary>
+    public class FileStorage
+    {
+        private string _directoryPath;
+
+        public string DirecoryPath { get { return _directoryPath; } }
+
+        public FileStorage(string directoryPath)
+        {
+            _directoryPath = directoryPath;
+        }
+
+        public File Get(string fileName)
+        {
+            return new File(string.Format("{0}/{1}", _directoryPath, fileName));
+        }
+
+        public IQueryable<File> Get(string[] fileNames)
+        {
+            var list = new List<File>();
+
+            foreach (var fileName in fileNames)
+            {
+                list.Add(Get(fileName));
+            }
+
+            return list.AsQueryable<File>();
+        }
+
+        public void Save(File file)
+        {
+            file.Save(_directoryPath);
+        }
+
+        public bool Delete(File file)
+        {
+            return Delete(file.Name);
+        }
+
+        public bool Delete(string fileName)
+        {
+            if (IOFile.Exists(fileName))
+            {
+                IOFile.Delete(fileName);
+            }
+
+            return true;
+        }
+    }
+}
