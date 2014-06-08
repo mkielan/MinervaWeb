6a5630d20dbd8cd9dc0e415ccc64fe55f3d726e5
Mariusz Kielan <mariusz@kielan.org>
2014-06-08 12:43:37 +0200

    Api models

---
+++ Minerva.Models/Api/Directory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva.Models.Api
+{
+    public class Directory : StorageItem
+    {
+    }
+}
---
+++ Minerva.Models/Api/StorageItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva.Models.Api
+{
+    public abstract class StorageItem
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        public long ParentId { get; set; }
+
+        [MaxLength(500)]
+        public string Path { get; set; }
+    }
+}
