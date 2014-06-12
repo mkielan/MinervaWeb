b267bafe23c53fb935a72e1132e65f1003c2314a
Mariusz Kielan <mariusz@kielan.org>
2014-06-12 21:55:07 +0200

    Added missing file at entities

---
+++ Minerva.Entities/AbstractFullEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva.Entities
+{
+    public class AbstractFullEntity : AbstractEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public ApplicationUser CreatedBy { get; set; }
+
+        public DateTime? ModificationTime { get; set; }
+
+        public ApplicationUser ModifiedBy { get; set; }
+
+        public DateTime? DeletedTime { get; set; }
+
+        public ApplicationUser DeletedBy { get; set; }
+    }
+}
