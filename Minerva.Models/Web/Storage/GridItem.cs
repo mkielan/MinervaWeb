720772255960890cde2e6e442811cabaf7160b1b
Mariusz Kielan <mariusz@kielan.org>
2014-06-12 22:18:43 +0200

    Added test grid for storage

---
+++ Minerva.Models/Web/Storage/GridItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva.Models.Web.Storage
+{
+    
+    public class GridItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? LastModification { get; set; }
+
+        public ItemType Type { get; set; }
+    }
+}
--- Minerva/Controllers/StorageController.cs
+++ Minerva/Controllers/StorageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minerva.Models.Web.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,24 @@ using System.Web.Mvc;
 
 namespace Minerva.Controllers
 {
+    [Authorize]
     public class StorageController : Controller
     {
         // GET: Storage
         public ActionResult Index()
         {
-            return View();
+            var test = new List<GridItem>();
+            test.AddRange(
+                new [] { 
+                    new GridItem {Id = 1, Name = "Test1", Type = ItemType.Directory },
+                    new GridItem {Id = 2, Name = "Test2", Type = ItemType.Directory },
+                    new GridItem {Id = 3, Name = "Test3", Type = ItemType.Directory },
+                    new GridItem {Id = 4, Name = "File1", Type = ItemType.File, LastModification = DateTime.Now.AddHours(-15) },
+                    new GridItem {Id = 5, Name = "File2", Type = ItemType.File, LastModification = DateTime.Now.AddHours(-25) },
+                    new GridItem {Id = 6, Name = "File3", Type = ItemType.File, LastModification = DateTime.Now.AddHours(-3) }
+                });
+
+            return View(test);
         }
     }
 }
