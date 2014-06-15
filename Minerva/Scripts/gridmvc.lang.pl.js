7fd89fcf4314838895f72dae8ffdd4fe3cad75a7
Mariusz Kielan <mariusz@kielan.org>
2014-06-15 21:36:48 +0200

    Added polish language for grid.mvc

---
+++ Minerva/Scripts/gridmvc.lang.pl.js
@@ -0,0 +1,22 @@
+﻿/***
+* Grid.Mvc polish language pl-PL)
+*/
+window.GridMvc = window.GridMvc || {};
+window.GridMvc.lang = window.GridMvc.lang || {};
+GridMvc.lang.ru = {
+    filterTypeLabel: "Typ: ",
+    filterValueLabel: "Wartość:",
+    applyFilterButtonText: "Zastosuj",
+    filterSelectTypes: {
+        Equals: "Równe",
+        StartsWith: "Zaczyna się na",
+        Contains: "Zawiera",
+        EndsWith: "Kończy się na",
+        GreaterThan: "Większe niż",
+        LessThan: "Mniejsze niż"
+    },
+    code: 'pl',
+    boolTrueLabel: "Tak",
+    boolFalseLabel: "Nie",
+    clearFilterLabel: "Wyczyść filtry"
+};
