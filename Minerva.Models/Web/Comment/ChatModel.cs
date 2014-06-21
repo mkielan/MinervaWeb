b6d73878cb2e9db0bf9c9a1a537086a69b7a4fa7
Mariusz Kielan <mariusz@kielan.org>
2014-06-22 01:07:57 +0200

    Aded models for breadcrumb and comments

---
+++ Minerva.Models/Web/Comment/ChatModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva.Models.Web.Comment
+{
+    public class ChatModel
+    {
+        public int ItemId { get; set; }
+
+        public string UserName { get; set; }
+
+        public ICollection<Item> Messages { get; set;}
+    }
+}
---
+++ Minerva.Models/Web/Storage/BreadcrumbItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minerva.Models.Web.Storage
+{
+    public class BreadcrumbItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
