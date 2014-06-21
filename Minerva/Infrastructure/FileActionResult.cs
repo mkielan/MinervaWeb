2293aadf39b1b0360d0287188ebf1b5c7b669d0a
Mariusz Kielan <mariusz@kielan.org>
2014-06-21 22:22:38 +0200

    Add new types of results to infrastructure

---
+++ Minerva/Infrastructure/FileActionResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Minerva.Infrastructure
+{
+    public class FileActionResult : IHttpActionResult
+    {
+        private string _path;
+
+        public FileActionResult(string path)
+        {
+            _path = path;
+        }
+
+        public int FileId { get; private set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            if (!File.Exists(_path))
+            {
+                return Task.FromResult(new HttpResponseMessage(HttpStatusCode.NotFound));
+            }
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StreamContent(File.OpenRead(_path));
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ Minerva/Infrastructure/HttpStatusCodeWithMessageResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Minerva.Infrastructure
+{
+    public class HttpStatusCodeWithMessageResult : IHttpActionResult
+    {
+        private string _message;
+        private HttpStatusCode _code;
+
+        public HttpStatusCodeWithMessageResult(HttpStatusCode code, string message = null)
+        {
+            _message = message;
+            _code = code;
+        }
+    
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(_code);
+
+            if(!string.IsNullOrEmpty(_message)) response.Content = new StringContent(_message);
+
+            return Task.FromResult(response);
+        }
+    }
+}
