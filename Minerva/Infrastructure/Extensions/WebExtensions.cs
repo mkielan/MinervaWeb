1e89a6fec28316521c3df5d050031d11c7ca0d76
Mariusz Kielan <mariusz@kielan.org>
2014-06-22 21:23:17 +0200

    Added web extension

---
+++ Minerva/Infrastructure/Extensions/WebExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Minerva.Infrastructure.Extensions
+{
+    public static class WebExtensions
+    {
+        public static string ControllerName(this WebViewPage @this)
+        {
+            return HttpContext.Current.Request.RequestContext.RouteData.Values["controller"].ToString();
+        }
+
+        public static string ActionName(this WebViewPage @this)
+        {
+            return HttpContext.Current.Request.RequestContext.RouteData.Values["action"].ToString();
+        }
+    }
+}
